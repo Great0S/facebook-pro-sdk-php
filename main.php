ff90edf715c6989bc2df4652ae797b7200a27f20
CommandGeneral <greatos@outlook.com>
2022-06-06 11:54:54 +0300

    update

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Simple Facebook Login
+ * Description: This plugin adds facebook login button anywhere in your wordpress website using a shortcode
+ * Plugin URI: https://github.com/Great0s
+ * Author: GreatOs
+ * Version: 0.0.1
+ **/
+
+ //* Don't directly access this file
+ defined( 'ABSPATH' ) or die
